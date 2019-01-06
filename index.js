deb767a0991b2f2005a4f6f9361ab06266cf8656
NikitaKemarskiy <sashapobeditel1980@gmail.com>
2019-01-06 10:48:09 +0200

    Icons folder was changed. CSS styles had little changes.

--- index.js
+++ index.js
@@ -7,58 +7,101 @@ const electron = require('electron');
 const ipcHandlers = require(path.join(process.cwd(), 'api', 'ipcHandlers', 'ipcHandlers.js'));
 
 // Constants
-const iconPath = path.join(process.cwd(), 'icons', 'icon.png');
+const icon64x64 = path.join(process.cwd(), 'icons', 'png', '64x64.png');
 
 // Electron
 const app = electron.app;
 const ipc = electron.ipcMain;
+const Tray = electron.Tray;
+const Menu = electron.Menu;
 const BrowserWindow = electron.BrowserWindow;
 
-let win = null;
+// Main window variable
+let mainWindow = null;
 
 // Functions
 const createWindow = function() {
-	win = new BrowserWindow({ 
+	mainWindow = new BrowserWindow({ 
 		width: 600, 
-		height: 700,  
+		height: 700,
+		center: true,
 		frame: true, 
 		show: false,
-		icon: iconPath,
+		icon: icon64x64,
 		title: 'YouTube to mp3' 
 	});
 
-	win.loadURL(url.format({
+	mainWindow.loadURL(url.format({
 		pathname: path.join(process.cwd(), 'public', 'html', 'index.html'),
 		protocol: 'file',
 		slashes: true
 	}));
 
-	win.once('ready-to-show', function() {
-		win.show();
+	mainWindow.once('ready-to-show', function() {
+		mainWindow.show();
 	});
 
-	win.on('closed', function() {
-		win = null;
+	mainWindow.on('closed', function() {
+		mainWindow = null;
 	});
 }
 
 // Electron events
 app.on('ready', function() {
-	createWindow();
+	const menu = Menu.buildFromTemplate(mainMenuTemplate); // Build an application menu
+	Menu.setApplicationMenu(menu); // Set this menu as an application menu
+	new Tray(icon64x64); // Set an application tray
+
+	createWindow(); // Create an application window
 });
 
 app.on('window-all-closed', function() {
-	if (process.platform !== 'darwin') {
-		app.quit();
-	}
-});
-
-app.on('activate', function() {
-	if (window === null) {
-		createWindow();
-	}
+	app.quit();
 });
 
 // IPC events
 ipc.on('searchUrl', ipcHandlers.searchUrl);
 ipc.on('downloadVideo', ipcHandlers.downloadVideo);
+
+// Main application menu template
+const mainMenuTemplate = [
+	{
+		label: 'File',
+		submenu: [
+			{
+				label: 'Quit',
+				click: function() {
+					app.quit();
+				},
+				accelerator: 'CmdOrCtrl + Q'
+			}
+		]
+	},
+	{
+		label: 'Edit',
+		submenu: [
+			{ role: 'undo' }, 
+			{ role: 'redo' }, 
+			{ type: 'separator' }, 
+			{ role: 'cut' }, 
+			{ role: 'copy' }, 
+			{ role: 'paste' }, 
+			{ role: 'pasteandmatchstyle' }, 
+			{ role: 'delete' }, 
+			{ role: 'selectall' }, 
+		]
+	},
+	{
+		label: 'Help',
+		submenu: [
+			{ role: 'toggleDevTools' },
+			{
+				label: 'About electron',
+				click: function() {
+					electron.shell.openExternal('https://electronjs.org/');
+				},
+				accelerator: 'CmdOrCtrl + Shift + H'
+			}
+		]
+	}
+];
